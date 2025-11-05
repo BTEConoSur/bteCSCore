534e85f8cda383d91c4c847647fa17ebc935a47f
MatiPoli <matiastmarquez@gmail.com>
2025-11-05 16:12:43 -0300

    add discord manager and bot config

---
+++ src/main/java/com/bteconosur/discord/BotConfig.java
@@ -0,0 +1,76 @@
+package com.bteconosur.discord;
+
+import org.bukkit.configuration.file.YamlConfiguration;
+
+import com.bteconosur.core.BTEConoSur;
+import com.bteconosur.core.config.ConfigHandler;
+import com.bteconosur.core.utils.ConsoleLogger;
+
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+
+public class BotConfig {
+
+    private final YamlConfiguration config;
+    private final YamlConfiguration lang;
+    private final ConsoleLogger logger;
+
+    private JDA jda;
+
+    public BotConfig() {
+        ConfigHandler configHandler = ConfigHandler.getInstance();
+        config = configHandler.getConfig();
+        lang = configHandler.getLang();
+        logger = BTEConoSur.getConsoleLogger();
+    }
+
+    public void startBot() {
+        if (jda != null) {
+            logger.warn("El Bot de Discord ya está iniciado. Usar restartBot() para reiniciarlo.");
+        }
+        
+        logger.info(lang.getString("discord-bot-initializing"));
+        try {
+            jda = JDABuilder.createDefault(config.getString("discord-bot-token"))
+                    .enableIntents(GatewayIntent.MESSAGE_CONTENT)
+                    .enableIntents(GatewayIntent.GUILD_PRESENCES)
+                    .setActivity(Activity.playing("Testando bot para Cono Sur"))
+                    .build().awaitReady();
+        } catch (Exception e) {
+            logger.error("Error al iniciar el Bot de Discord: " + e);
+        }
+    }
+
+    public void stopBot() {
+        logger.info(lang.getString("discord-bot-shutting-down"));
+        if (jda == null) {
+            logger.warn("El Bot de Discord no está iniciado.");
+            return;
+        }
+        
+        jda.shutdown();
+        try {
+            jda.awaitShutdown();
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            logger.error("Error al esperar el apagado del Bot de Discord: " + e);
+        }
+        jda = null;
+        
+    }
+
+    public void restartBot() {
+        logger.info(lang.getString("discord-bot-restarting"));
+        stopBot();
+        startBot();
+    }
+
+    public JDA getJDA() {
+        if (jda == null) {
+            logger.error("JDA no inicializado.");
+        }
+        return jda;
+    }
+}
---
+++ src/main/java/com/bteconosur/discord/DiscordManager.java
@@ -0,0 +1,41 @@
+package com.bteconosur.discord;
+
+import org.bukkit.configuration.file.YamlConfiguration;
+
+import com.bteconosur.core.BTEConoSur;
+import com.bteconosur.core.config.ConfigHandler;
+import com.bteconosur.core.utils.ConsoleLogger;
+
+import net.dv8tion.jda.api.JDA;
+
+public class DiscordManager {
+
+    private final YamlConfiguration lang;
+    private final ConsoleLogger logger;
+
+    private BotConfig botConfig;
+
+    public DiscordManager() {
+        ConfigHandler configHandler = ConfigHandler.getInstance();
+        lang = configHandler.getLang();
+        logger = BTEConoSur.getConsoleLogger();
+      
+        logger.info(lang.getString("discord-initializing"));
+
+        botConfig = new BotConfig();
+        botConfig.startBot();
+    }
+
+    public void restartBot() {
+        botConfig.restartBot();
+    }
+
+    public JDA getJda() {
+        return botConfig.getJDA();
+    }
+
+    public void shutdown() {
+        logger.info(lang.getString("discord-shutting-down"));
+        botConfig.stopBot();
+    }
+}
