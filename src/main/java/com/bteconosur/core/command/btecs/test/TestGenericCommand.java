3184fc286af38d3e68062382f6d6513162af0684
Matias Tomás Marquez <matiastmarquez@gmail.com>
2026-02-03 17:44:54 -0300

    add SignGUI dependency

--- src/main/java/com/bteconosur/core/command/btecs/test/TestGenericCommand.java
+++ src/main/java/com/bteconosur/core/command/btecs/test/TestGenericCommand.java
@@ -1,12 +1,16 @@
 package com.bteconosur.core.command.btecs.test;
 
+import java.util.Collections;
+
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.HumanEntity;
+import org.bukkit.entity.Player;
 
 import com.bteconosur.core.command.BaseCommand;
+import com.bteconosur.core.util.ConsoleLogger;
 
-import dev.triumphteam.gui.guis.Gui;
-import net.kyori.adventure.text.minimessage.MiniMessage;
+import de.rapha149.signgui.SignGUI;
+import de.rapha149.signgui.SignGUIAction;
+import de.rapha149.signgui.exception.SignGUIVersionException;
 
 
 public class TestGenericCommand extends BaseCommand {
@@ -17,12 +21,25 @@ public class TestGenericCommand extends BaseCommand {
 
     @Override
     protected boolean onCommand(CommandSender sender, String[] args) {
-        Gui gui = Gui.gui()
-                .title(MiniMessage.miniMessage().deserialize("<gradient:#FF0000:#0000FF>Comando Genérico de Prueba</gradient>"))
-                .rows(3)
-                .create();
-
-        gui.open((HumanEntity) sender);
+        Player player = (Player) sender;
+        
+        SignGUI gui;
+        try {
+            gui = SignGUI.builder()
+                    .setLines("","","^^^^^^^^^", "Escribe arriba")
+                    .setHandler((p, result) -> {
+                        String input1 = result.getLineWithoutColor(0);
+                        String input2 = result.getLineWithoutColor(1);
+                        player.sendMessage("§aEscribiste: §f" + input1 + input2);
+                        return Collections.singletonList(SignGUIAction.run(() -> player.sendMessage("§eSign GUI cerrado")));
+                    })
+                    .build();
+                gui.open(player);
+        } catch (SignGUIVersionException e) {
+            ConsoleLogger.error("Error al crear el SignGUI: " + e.getMessage());
+            e.printStackTrace();
+        }
+        
         return true;
     }
 
